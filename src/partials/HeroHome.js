d9ebdfe8cb5d5b57cab8a2a0be5d4b81a0d1560f
peter westman <pwestman@gmail.com>
2020-09-30 11:57:55 +0200

    install and update

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -27,7 +27,7 @@ function HeroHome() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">Landing template for startups</h1>
+            <h1 className="h1 mb-4" data-aos="fade-up">Circular</h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0" data-aos="fade-up" data-aos-delay="400">Start free trial</a>
